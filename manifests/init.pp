f6897f98ba4ca27660fb56f423d1b708914c1800
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-09 18:56:33 +0100

    Use the openjdk instead of all the oracle fuss.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,15 @@
 class puppet-java {
 
-  $ppa_repository = 'ppa:webupd8team/javtxa'
+  $java_packages = ['openjdk-6-jdk', 'openjdk-6-source', 'openjdk-7-jdk', 'openjdk-7-source']
 
-  $needed_packages = ['aptitude', 'python-software-properties']
-
-  Package[$needed_packages] -> Exec['add-repo'] -> Exec['aptitude-update'] -> Package['java']
-
-  package {$needed_packages:
-    ensure => 'latest',
-  }
-
-  exec {'aptitude-update':
-    command => '/usr/bin/aptitude update',
-  }
-
-  exec { 'add-repo':
-    command => "/usr/bin/add-apt-repository $ppa_repository"
-  }
+  # exec { 'update-alternatives':
+  #   command => 'update-java-alternatives -s sun-java6-jdk'
+  # }
 
   # install the package
-  package {['sun-java6-jdk' 'sun-java6-source' 'sun-java6-plugin']:
+  package {$java_packages:
     ensure   => 'latest',
     provider => 'aptitude',
   }
+
 }
